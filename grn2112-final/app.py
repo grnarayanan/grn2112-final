9456f781c820b214534f0b218d151502e1a14429
Ganesan Narayanan <ganesan.n@columbia.edu>
2020-05-11 15:29:22 -0500

    Finished homepage

--- grn2112-final/app.py
+++ grn2112-final/app.py
@@ -11,15 +11,21 @@ from flask import Flask, render_template
 #Flask app variable
 app = Flask(__name__)
 
-#static route
+#home page route
 @app.route("/")
 def hello():
     return render_template("index.html")
 
-#engi1006 route
-@app.route("/1006")
-def engi1006():
-    return "1006 homepage"
+#assignments page route
+@app.route("/assignments/")
+def assignments():
+    return "Welcome to the Assignments page!"
+
+#classes page route
+@app.route("/classes/")
+def classes():
+    return "Welcome to the Classes page!"
+
 
 #start the server
 if __name__ == "__main__":
